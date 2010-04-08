d239464e15005fc99fb0b7e55415bc9bda2ad7f3
khagan <khagan@scylla.(none)>
2010-04-08 17:05:40 -0400

    empty django project

---
+++ settings_production.py
@@ -0,0 +1,10 @@
+from settings_shared import *
+
+TEMPLATE_DIRS = (
+    "/var/www/smilekit/smilekit/templates",
+)
+
+MEDIA_ROOT = '/var/www/smilekit/uploads/'
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
