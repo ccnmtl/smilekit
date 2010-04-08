2244bb65ae73865d470b0d09f09dcf7278248fde
khagan <khagan@scylla.(none)>
2010-04-08 18:59:04 -0400

    merged marc's work into django project

--- collection_tool/views.py
+++ collection_tool/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 def index(request):
-    return render_to_response("collection_tool_index.html")
+    return render_to_response("index.html")
