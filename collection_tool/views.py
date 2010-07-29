d23a060650fa7b9248771488e6eb6ae647b3c021
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2010-07-29 14:19:24 -0400

    new collection tool templates

--- collection_tool/views.py
+++ collection_tool/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
-def index(request):
-    return render_to_response("collection_tool/index.html")
+#def index(request):
+#    return render_to_response("collection_tool/index.html")
 
 
-def temp_html(request):
-    return render_to_response("collection_tool/temp_html.html")
+#def temp_html(request):
+#    return render_to_response("collection_tool/temp_html.html")
+    
+
+def question(request, question_id, language_code):
+    return render_to_response("collection_tool/question.html")
+    
