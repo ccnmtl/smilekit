59345a4e2621b7850f1f5a85a00b9dee8d890529
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2010-08-04 13:59:18 -0400

    basic working interview with caching

--- collection_tool/models.py
+++ collection_tool/models.py
@@ -12,21 +12,88 @@ Question = models.get_model('equation_balancer', 'question')
 Answer = models.get_model('equation_balancer', 'answer')
   
 class DisplayQuestion(models.Model):
-  """associates questions with images, help topics, etc."""
+  """associates questions with translations, images, help topics, etc."""
   part_of_score =  models.CharField(max_length=1, null=False)
   question = models.ForeignKey(Question, null=True, blank=True)
   #topic = models.ForeignKey(Topic, blank=True, null=True)
   image = models.ImageField(upload_to='question_images',blank=True,null=True)
-  # order? -- would control display within topic
-  #add a method for englishtranslation()
-  #add a method for spanishtranslation()
+  
+  @models.permalink
+  def get_absolute_url(self):
+    return ('smilekit.views.question', [str(self.id), 'en'])
 
-  def __unicode__(self):
-    #import pdb
-    #pdb.set_trace()
-    if self.translation_set.all():
+  
+  @property
+  def display_answers (self):
+    return DisplayAnswer.objects.filter(answer__in= self.question.answer_set.all())
+
+  
+  @property
+  def answers(self):
+      return self.question.answer_set.all()
+  
+  
+  @property
+  def next(self):
+    all_numbers = [q.number for q in Question.objects.all()]
+    my_number = self.question.number
+    next_index = all_numbers.index(my_number) + 1
+    try:
+      next_question = Question.objects.get(number=all_numbers[next_index])
+      return next_question.displayquestion_set.all()[0]
+    except:
+      return None
+  
+  
+  @property
+  def prev(self):
+    all_numbers = [q.number for q in Question.objects.all()]
+    my_number = self.question.number
+    prev_index = all_numbers.index(my_number) - 1
+    try:
+      prev_question = Question.objects.get(number=all_numbers[prev_index])
+      return prev_question.displayquestion_set.all()[0]
+    except:
+      return None
+  
+
+  @property
+  def english(self):
+    try:
+      return Translation.objects.get(question=self, language='en').text
+    except:
+      return None
+
+  @property
+  def spanish(self):
+    try:
+      return Translation.objects.get(question=self, language='es').text
+    except:
+      return None
+
+  
+  def wording(self, preferred_language_code):
+    """attempts to provide a wording in preferred language; otherwise return a language in an arbitrary language; otherwise returns a fixed string."""
+    try:
+      return Translation.objects.get(question=self, language=preferred_language_code).text
+    except:
+      pass
+    
+    
+    try:
       return self.translation_set.all()[0].text
-
+    except:
+      pass    
+    
+    return "Sorry, no wordings provided in either language. Enter wordings at /admin/collection_tool/displayquestion/%d/" % self.id
+  
+  
+  def __unicode__(self):
+    """ Just the English."""
+    try:
+      return self.english
+    except:   
+      return None
 
 class Translation(models.Model):
   """ a question wording in a particular language"""
@@ -37,17 +104,49 @@ class Translation(models.Model):
 class DisplayAnswer(models.Model):
   """associates answers with images, help topics, etc."""
 
+  #TODO: this isn't right: this should be a one-to-one mapping. there should only be one display answer per answer. this shouln't be a foreign key.
   answer = models.ForeignKey(Answer)  # better to link to DB answer or DisplayQuestion?
+  
   image = models.ImageField(upload_to='answer_images',blank=True,null=True)
   # order -- how to display within question
   
-  #add a method for englishtranslation()
-  #add a method for spanishtranslation()
+  
+  
   def question_text (self):
     return self.answer.question.text
   
   def answer_text (self):
     return self.answer.text  
+    
+  @property
+  def english(self):
+    try:
+      return AnswerTranslation.objects.get(answer=self, language='en').text
+    except:
+      return None
+
+  @property
+  def spanish(self):
+    try:
+      return AnswerTranslation.objects.get(answer=self, language='es').text
+    except:
+      return None
+  
+  
+  def wording(self, preferred_language_code):
+    """attempts to provide a wording in preferred language; otherwise falls back on the other language; otherwise returns a fixed string."""
+    try:
+      return AnswerTranslation.objects.get(answer=self, language=preferred_language_code).text
+    except:
+      pass
+    
+    
+    try:
+      return self.answer_translation_set.all()[0].text
+    except:
+      pass    
+    
+    return "Sorry, no wordings provided in either language. Enter wordings at /admin/collection_tool/displayanswer/%d/" % self.id
     
   
 class AnswerTranslation(models.Model):
--- collection_tool/views.py
+++ collection_tool/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
+from smilekit.collection_tool.models import *
+from django.shortcuts import get_object_or_404, render_to_response
+from django.template import RequestContext, loader
 
 #def index(request):
 #    return render_to_response("collection_tool/index.html")
@@ -9,6 +12,84 @@ from django.shortcuts import render_to_response
 #    return render_to_response("collection_tool/temp_html.html")
     
 
-def question(request, question_id, language_code):
-    return render_to_response("collection_tool/question.html")
+def question(request, displayquestion_id, language_code):
+  """ Look up a DisplayQuestion object and display it in the data collection tool. Note that question_id refers to a displayquestion object, not a question object; some displayquestions are not associated with any question."""
+  #print displayquestion_id
+  #print language_code
+  #print DisplayQuestion.objects.get(id=displayquestion_id)
+  #if language_
+  
+  displayquestion = get_object_or_404(DisplayQuestion, pk=displayquestion_id)
+  
+  if language_code not in ['en', 'es']:
+    raise Http404
+  
+  wording = None
+  if language_code == 'en':
+    wording = displayquestion.english
+  if language_code == 'es':
+    wording = displayquestion.spanish
+  
+  #import pdb
+  #pdb.set_trace()
+  
+  #if wording == None:
+  #  raise Http404
+  #  #TODO: use backup wording in the other language if only one is available.
+  
+  
+  #TODO: how the hell do we store attach answers to display_questions that have a null question?
+  wording = displayquestion.wording(language_code)
+  
+  
+  answer_wordings = [d.wording(language_code) for d in displayquestion.display_answers]
+  
+  #return render_to_response("collection_tool/question.html")
+  t = loader.get_template('collection_tool/question.html')
+  c = RequestContext(request,{
+      'displayquestion': displayquestion,
+      'wording' : wording,
+      'answer_wordings': answer_wordings,
+      'language_code': language_code
+      #'answers': None
+  })
+  return HttpResponse(t.render(c))
     
+
+def html_sandbox(request):
+  return render_to_response("collection_tool/html_sandbox.html")
+
+# for testing:
+def available_offline(request):
+  t = loader.get_template('collection_tool/message.html')
+  c = RequestContext(request,{
+      'message': "This page should be available offline."
+  })
+  return HttpResponse(t.render(c))
+
+def not_available_offline(request):
+  t = loader.get_template('collection_tool/message.html')
+  print "NOT AVAILABLE OFFLINE"
+  c = RequestContext(request,{
+      'message': "This page should NOT be available offline."
+  })
+  return HttpResponse(t.render(c))
+
+
+def manifest(request):
+  """ This is the list of files that Smilekit needs to save locally on the ipad, so that researchers can access them offline while interviewing.
+  The url is:
+  /collection_tool/manifest.cache
+  Relevant research:
+  https://developer.mozilla.org/en/Offline_resources_in_Firefox
+  http://docs.djangoproject.com/en/dev/ref/request-response/
+  http://stackoverflow.com/questions/1715568/how-to-properly-invalidate-an-html5-cache-manifest-for-online-offline-web-apps
+  http://www.webreference.com/authoring/languages/html/HTML5-Application-Caching/
+  """
+  response = HttpResponse(mimetype='text/cache-manifest')
+  t = loader.get_template('collection_tool/manifest')
+  c = RequestContext(request,{
+    'question_ids': [d.id for d in DisplayQuestion.objects.all()]
+  })
+  response.write(t.render(c))
+  return response
