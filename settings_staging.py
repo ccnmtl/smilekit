b09983154b9ce58881dce296e7cca172e508feb0
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2010-11-26 12:03:40 -0500

    adding staging conf files (1)

---
+++ settings_staging.py
@@ -0,0 +1,10 @@
+from settings_shared import *
+
+TEMPLATE_DIRS = (
+    "/usr/local/share/sandboxes/common/smilekit/templates",)
+
+MEDIA_ROOT = '/var/www/smilekit/uploads/'
+ADMIN_MEDIA_PREFIX = '/site_media/admin/'
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
