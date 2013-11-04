863a3a78179d4799057e8f331324bd44bb2e0604
Anders Pearson <anders@columbia.edu>
2013-11-04 10:31:48 -0500

    convert to django 1.4 directory structure

--- smilekit/settings_staging.py
+++ smilekit/settings_staging.py
@@ -1,6 +1,6 @@
 from settings_shared import *
 
-TEMPLATE_DIRS = ( "/usr/local/share/sandboxes/common/smilekit/templates")
+TEMPLATE_DIRS = ( "/usr/local/share/sandboxes/common/smilekit/smilekit/templates")
 
 MEDIA_ROOT = '/var/www/smilekit/uploads/'
 ADMIN_MEDIA_PREFIX = '/site_media/admin/'
--- smilekit/urls.py
+++ smilekit/urls.py
@@ -4,11 +4,11 @@ from django.conf import settings
 import os.path
 admin.autodiscover()
 
-site_media_root = os.path.join(os.path.dirname(__file__),"media")
+site_media_root = os.path.join(os.path.dirname(__file__),"../media")
 
 urlpatterns = patterns(
   '',                  
-  (r'^$', 'family_info.views.families'),
+  (r'^$', 'smilekit.family_info.views.families'),
   (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/site_media/images/favicon.ico'}),
   (r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'logged_out.html'}),
   ('^accounts/',include('djangowind.urls')),
@@ -16,9 +16,9 @@ urlpatterns = patterns(
   (r'^tinymce/', include('tinymce.urls')),
   (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': site_media_root}),
   (r'^uploads/(?P<path>.*)$','django.views.static.serve',{'document_root' : settings.MEDIA_ROOT}),
-  (r'^weights/', include('equation_balancer.urls')),
-  (r'^collection_tool/', include('collection_tool.urls')),
-  (r'^family_info/', include('family_info.urls')),
+  (r'^weights/', include('smilekit.equation_balancer.urls')),
+  (r'^collection_tool/', include('smilekit.collection_tool.urls')),
+  (r'^family_info/', include('smilekit.family_info.urls')),
   (r'^logout$', 'django.contrib.auth.views.logout',
     {
       'template_name': 'family_info/logged_out.html',
