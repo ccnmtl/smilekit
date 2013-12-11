fddfb26da5f4a864756ab4803095d613369f23cd
Anders Pearson <anders@columbia.edu>
2013-12-11 17:18:59 -0500

    a view test to get started

--- smilekit/family_info/tests/test_views.py
+++ smilekit/family_info/tests/test_views.py
@@ -1,8 +1,23 @@
 from django.test import TestCase
+from django.test.client import Client
 from smilekit.family_info.views import default_family_form_vars
+from .factories import UserFactory
 
 
 class DefaultFamilyFormVarsTest(TestCase):
     def test_basics(self):
         r = default_family_form_vars()
         self.assertEqual(r['equation_balancer_configs'].count(), 0)
+
+
+class LoggedInViewsTests(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.u = UserFactory()
+        self.u.set_password("test")
+        self.u.save()
+        self.c.login(username=self.u.username, password="test")
+
+    def test_families(self):
+        r = self.c.get("/family_info/families/")
+        self.assertEqual(r.status_code, 200)
